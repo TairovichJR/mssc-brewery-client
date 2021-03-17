aa87bec8414648f11373e08fc732617737cc5e6f
TairovichJR <yokubov.omonjon@gmail.com>
2021-03-16 21:32:01 -0400

    set up for client

---
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,17 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BreweryClientTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getBeerId() {
+    }
+}
