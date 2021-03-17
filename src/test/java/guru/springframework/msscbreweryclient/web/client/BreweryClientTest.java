f89a2dbc6b5cf9897c744b1b2346c219bdb4142b
TairovichJR <yokubov.omonjon@gmail.com>
2021-03-16 22:10:20 -0400

    get request with rest template

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -1,17 +1,27 @@
 package guru.springframework.msscbreweryclient.web.client;
 
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class BreweryClientTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @Autowired
+    BreweryClient client;
 
     @Test
     void getBeerId() {
+        BeerDto dto = client.getBeerId(UUID.randomUUID());
+        assertEquals("Pale Ale", dto.getBeerStyle());
+        assertEquals("Galaxy Cat", dto.getBeerName());
+        assertEquals(1234L, dto.getUpc());
+        assertNotNull(dto);
     }
 }
